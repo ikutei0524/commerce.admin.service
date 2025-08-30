5fdda68342198986afe18890ba15ea0cdacd2fd9
ikutei0524 <ikutei0524@gmail.com>
2025-08-30 21:08:26 +0800

    Controllers

---
+++ src/main/java/com/YuTing/commerce/admin/service/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.YuTing.commerce.admin.service.controllers;
+
+
+import com.YuTing.commerce.admin.service.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+
+
+
+
+
+
+
+}
